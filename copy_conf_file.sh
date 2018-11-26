47c1edfbcbb2183c163f888e56d5c41958e41293
firer1946 <firer1946@233.com>
2018-11-26 15:04:21 +0800

    233

--- copy_conf_file.sh
+++ copy_conf_file.sh
@@ -4,4 +4,4 @@ cp -f ~/.tmux/.tmux.conf.local ~/
 chmod -R +x ~/.tmux/tmux-resurrect/scripts/
 chmod -R +x ~/.tmux/tmux-resurrect/resurrect.tmux
 chmod -R +x ~/.tmux/tmux-continuum/scripts/
-chmod -R +x ~/.tmux/tmux-resurrect/continuum.tmux
+chmod -R +x ~/.tmux/tmux-continuum/continuum.tmux
